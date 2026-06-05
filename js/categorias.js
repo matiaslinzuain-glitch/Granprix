b5967524ede9c32c5a090515b87f4564bed66c37
matiaslinzuain-glitch <matias.linzuain@gmail.com>
2026-06-05 20:54:14 -0300

    Admin: sistema completo de gestión de categorías (agregar, editar, eliminar con fotos)

---
+++ js/categorias.js
@@ -0,0 +1,9 @@
+// Categorías con foto y nombre
+const CATEGORIAS = [
+  { id: "camisas", nombre: "Camisas", imagen: "img/camisa-cuadros.jpeg" },
+  { id: "pantalones", nombre: "Pantalones", imagen: "img/cargo-negro-hombre.jpeg" },
+  { id: "buzos", nombre: "Buzos", imagen: "img/buzo-canguro-colores.jpeg" },
+  { id: "camperas", nombre: "Camperas", imagen: "img/campera-cuero-hombre.jpeg" },
+  { id: "sueters", nombre: "Suéters", imagen: "img/portada-sueters.jpeg" },
+  { id: "accesorios", nombre: "Accesorios", imagen: null }
+];
